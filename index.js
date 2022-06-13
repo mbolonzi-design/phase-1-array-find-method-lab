b17212ca289853f57712fee2deb9b89208f2d0a5
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-13 12:06:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    {year: "2020", result: "W"},
+    {year: "2019", result: "L"},
+    {year: "2021", result: "N/A"},
+    {year: "2018", result: "W"}
+]
+function superbowlWin(record){
+    let win = record.find(obj => obj.result === "W")
+
+    if(win){
+        return win.year
+    }
+}
